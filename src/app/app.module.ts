f48b7983015044e86b023d50227400b2cce7b71a
tanat <tanat87@gmail.com>
2017-10-23 19:36:50 +0300

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TransactionComponent } from './transaction/transaction.component';
+import { HomeComponent } from './home/home.component';
+import { AddressComponent } from './address/address.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TransactionComponent,
+    HomeComponent,
+    AddressComponent
   ],
   imports: [
     BrowserModule,
